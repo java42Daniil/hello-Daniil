e8de8a8b8a1eaa75d9548bf643b0a75a56f94584
Daniil Berezin <daniil.berezin98@gmail.com>
2021-10-08 17:00:17 +0300

    adds v1 to hw

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
-		System.out.println("HW is done as v2");
+		System.out.println("HW is done as v1");
 
 	}
 
