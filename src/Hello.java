097248e799cba7ce15f17b2dbfb538a512231fe9
Daniil Berezin <daniil.berezin98@gmail.com>
2021-10-08 17:05:49 +0300

    adds Merge v1-v2

--- src/Hello.java
+++ src/Hello.java
@@ -3,11 +3,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
-<<<<<<< HEAD
-		System.out.println("HW is done as v1");
-=======
-		System.out.println("HW is done as v2");
->>>>>>> refs/heads/hw-v2
+		System.out.println("HW is done as v1-v2");
 
 	}
 
